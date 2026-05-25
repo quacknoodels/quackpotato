48cca558344de1d136a29276df64ce533c224519
quack <57281423+quacknoodels@users.noreply.github.com>
2026-05-25 15:50:25 +0800

    implemented transition (unfixed)

--- public/js/anim.js
+++ public/js/anim.js
@@ -1,6 +1,17 @@
 //starting animation on site loadup
 $(document).ready(function(){
     //startup
+    /*$(".transition").animate({
+        left: "-200em",
+        width: "0",
+        height: "25rem"
+    }, "slow");
+    $(".transition").animate({
+        height: "0"
+    },"1");
+    */
+    var transitionSlides = $(".transition");
+    transitionAnim(transitionSlides);
     var titleCard = $(".title-card");
     titleCard.animate({ left: "0.5em" });
     titleCard.animate({ left: "-=1em", opacity: "1" }, "fast");
@@ -27,6 +38,17 @@ $(document).ready(function(){
         }
     )
 })
+
+function transitionAnim(slides) {
+    slides.eq(0).animate({
+        left: "-200em",
+        width: "0",
+        height: "25rem"
+    }, {duration:"slow", queue: false});
+    slides.eq(0).animate({ height: "0" },{duration:"slow", queue: false, complete: function(){
+        transitionAnim(slides.slice(1)); //slice off first element
+    }});
+}
 
 function optionAnim(options) {
     options.eq(0).animate({ left: "-=0.8em", opacity: "1" }, 150, function(){
